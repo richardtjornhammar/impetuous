ae07364f4448dfd159a5d2d4357473e0117c63f2
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2024-03-25 19:08:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "impetuous-gfa",
-    version = "0.101.14",
+    version = "0.101.15",
     author = "Richard Tjörnhammar",
     author_email = "richard.tjornhammar@gmail.com",
     description = "Impetuous Quantification, a Statistical Learning library for Humans : Alignments, Clustering, Fast NodeGraph Searching, Enrichments and Group Analysis",
