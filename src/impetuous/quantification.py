f28143c39af75ebd2643d474c1ebe976456036f1
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2025-10-30 22:18:00 +0100

    Update quantification.py

--- src/impetuous/quantification.py
+++ src/impetuous/quantification.py
@@ -2668,7 +2668,8 @@ def isItPrime( N , M=None,p=None,lM05=None ) :
 def areTheyIsomorphic( adjacency_matrix1, adjacency_matrix2 , TOL=1E-10 ):
     u1,s1,v1t = np.linalg.svd(adjacency_matrix1)
     u2,s2,v2t = np.linalg.svd(adjacency_matrix2)
-    return ( bool( np.sum(np.abs(vt)-np.abs(v2t))<TOL ) )
+    nm = np.prod(np.shape(adjacency_matrix1))
+    return ( bool( np.sum( (np.abs(v2t)-np.abs(v1t)) )/nm < TOL ) )
 
 # FIRST APPEARENCE:
 # https://gist.github.com/richardtjornhammar/ef1719ab0dc683c69d5a864cb05c5a90
