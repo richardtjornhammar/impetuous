8d01253b39e4a24703a672254a73d07ba8bfbda6
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2026-05-30 15:44:38 +0200

    Update spectral.py

--- src/impetuous/spectral.py
+++ src/impetuous/spectral.py
@@ -1,5 +1,5 @@
 """
-Copyright 2024 RICHARD TJÖRNHAMMAR
+Copyright 2026 RICHARD TJÖRNHAMMAR
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
